1c9a97d19c9ff1390dbf2fe492d1b8e4d23a8bc2
nutyfreshz <98257732+nutyfreshz@users.noreply.github.com>
2024-07-25 16:49:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 st.sidebar.header("Part 1.0) Upload XLSX Data")
-uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["xlsx"])
+uploaded_file = st.sidebar.file_uploader("Upload a Excel file", type=["xlsx"])
 
 st.sidebar.header("Part 1.1) Enter Sheet name")
 sheet_name_ex = st.sidebar.text_input("Enter Sheet name in Excel")
@@ -12,15 +12,15 @@ sheet_name_ex = st.sidebar.text_input("Enter Sheet name in Excel")
 df = pd.read_excel(uploaded_file
                      , sheet_name = sheet_name_ex)
 
-st.markdown("### Data Sample")
-st.write(df.head())
-
 st.sidebar.header("Part 1.2) Select Columns to Classify Topics")
 cols_option = st.sidebar.selectbox("Select Columns", df.columns.tolist())
 
 st.sidebar.header("Part 2) Enter Topics")
 text_label = st.sidebar.text_input("Enter Topics split with comma e.g. positive,negative")
 text_list = text_label.split(",")[:-1]
+
+st.markdown("### Data Sample")
+st.write(df.head())
 
 ###########################################################
 
